dc3a88148558a879e3bf3f027a31aa060fc36014
Tash Malik <TashfeenMalik10@hotmail.com>
2022-05-29 22:49:29 +0100

    Add project files.

---
+++ Object creator/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Object_creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.lbl_itemCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Black;
+            this.player.Location = new System.Drawing.Point(328, 167);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(51, 47);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // lbl_itemCount
+            // 
+            this.lbl_itemCount.AutoSize = true;
+            this.lbl_itemCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_itemCount.Location = new System.Drawing.Point(13, 13);
+            this.lbl_itemCount.Name = "lbl_itemCount";
+            this.lbl_itemCount.Size = new System.Drawing.Size(74, 20);
+            this.lbl_itemCount.TabIndex = 1;
+            this.lbl_itemCount.Text = "Items: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_itemCount);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Object Creator";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label lbl_itemCount;
+    }
+}
+
---
+++ Object creator/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Object_creator
+{
+    public partial class Form1 : Form
+    {
+        //Initialise variables
+
+        List<PictureBox> items = new List<PictureBox>();
+            Random rand = new Random();
+
+        int x;
+        int y;
+        int playerSpeed = 8;
+        int spawnTime = 20;
+
+        Color[] newColor = { Color.Red, Color.Turquoise, Color.Gold, Color.LimeGreen };
+
+        bool goUp;
+        bool goDown;
+        bool goLeft;
+        bool goRight;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MakePictureBox()
+        {
+            PictureBox new_pic = new PictureBox();
+            new_pic.Width = 30;
+            new_pic.Height = 30;
+            new_pic.BackColor = newColor[rand.Next(0, newColor.Length)];
+
+            x = rand.Next(10, this.ClientSize.Width - new_pic.Width);
+            y = rand.Next(10, this.ClientSize.Height - new_pic.Height);
+
+            new_pic.Location = new Point(x, y);
+
+            items.Add(new_pic); //Adds to the lsit
+
+            this.Controls.Add(new_pic); //Add to the form
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            //Handles player movement
+
+            if (goLeft)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goRight)
+            {
+                player.Left += playerSpeed;
+            }
+            if (goDown)
+            {
+                player.Top += playerSpeed;
+            }
+            if (goUp)
+            {
+                player.Top -= playerSpeed;
+            }
+
+            //shows total number of items left in the list
+            lbl_itemCount.Text = "Items: " + items.Count();
+
+            spawnTime -= 1;
+
+            if(spawnTime < 1)
+            {
+                MakePictureBox();
+                spawnTime = 20;
+            }
+
+            //Collision detection
+
+            foreach (PictureBox item in items.ToList())
+            {
+                if (player.Bounds.IntersectsWith(item.Bounds))
+                {
+                    items.Remove(item);
+                    this.Controls.Remove(item);
+                }
+
+            }
+        }
+
+        //When key is being pressed down the following method will allow movement
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+        }
+        //When key is released the following method will stop movement
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+        }
+    }
+}
